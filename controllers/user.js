ea900b6fa0ce9cfcf13497bb78a89fbffd261ecc
Hamle15 <80331657+Hamle15@users.noreply.github.com>
2023-04-04 11:01:35 -0500

    Making the register

--- controllers/user.js
+++ controllers/user.js
@@ -1,3 +1,4 @@
+const bcrypt = require("bcrypt");
 const User = require("../models/User");
 
 //ActionsTest
@@ -7,61 +8,119 @@ const pruebaUsers = (req, res) => {
   });
 };
 
-//Register of Users
-const register = async (req, res) => {
-
-  //Take data from the req
+const register3 = async (req, res) => {
+  //Take de info of teh body
   let params = req.body;
-
+  let hola = params.nick.toLowerCase();
+  let mial = params.email.toLowerCase();
+  console.log(hola);
+  console.log(mial);
 
   // Check that the data arrives correctly
-  if(!params.name ||!params.email || !params.password || !params.nick){
+  if (!params.name || !params.email || !params.password || !params.nick) {
     return res.status(400).json({
       status: "error",
-      message: "Missing data to send "
+      message: "Missing data to send ",
     });
   }
 
-  let user_to_save = new User(params);
-  // Control Users Duple
-  User.find({$or: [
-    {email: user_to_save.email.toLoweCase()},
-    {nick: user_to_save.nick.toLoweCase()},
-  ]}).exec()
-      .then((users) => {
-        if(users && users.length >= 1){
-          return res.status(200).send({
-            status: "succes",
-            message: "El usuario ya existe"
-          })
-        }else{
-          //encrypt password
+  try {
+    // Control Users Duple
+    const findDuplicate = await User.find({
+      $or: [{ email: params.email }, { nick: params.nick }],
+    }).exec();
 
-          //Save Users in the DB
+    console.log(findDuplicate);
 
-          // Return Res
+    if (findDuplicate && findDuplicate.length >= 1) {
+      return res.status(200).send({
+        status: "succes",
+        message: "El usuario ya existe",
+      });
+    }
 
+    //encrypt password
 
-          return res.status(200).json({
-            status: "succes",
-            message: "Action of register of users",
-            user_to_save
-          })
+    let passwordHash = await bcrypt.hash(params.password, 10);
+    params.password = passwordHash;
+
+    //Save DB
+
+    //Crate obj of the users
+    let user_to_save = new User(params);
+    let userStorage = await user_to_save.save();
+    if (!userStorage) {
+      return res.status(500).send({
+        status: "error",
+        message: "Error to save de User",
+      });
+    }
+
+    return res.status(200).json({
+      status: "succes",
+      message: "User Register correctly",
+      user: userStorage,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      status: "error",
+      message: "Error en la consulta",
+    });
+  }
+};
+
+const register = (req, res) => {
+  let params = req.body;
+
+  if (!params.name || !params.email || !params.password || !params.nick) {
+    return res.status(400).json({
+      status: "error",
+      message: "Missing data to send ",
+    });
+  }
+
+  User.find({
+    $or: [
+      { email: params.email.toLowerCase() },
+      { nick: params.nick.toLowerCase() },
+    ],
+  })
+    .exec()
+    .then(async (users) => {
+      if (users && users.length >= 1) {
+        return res.status(200).send({
+          status: "succes",
+          message: "El usuario ya existe",
+        });
+      }
+      let pdw = await bcrypt.hash(params.password, 10);
+      params.password = pdw;
+
+      let user_to_save = new User(params);
+
+      user_to_save.save().then((userStored) => {
+        if (!userStored) {
+          return res.status(500).send({
+            status: "error",
+            message: "Error to save de User",
+          });
         }
-
-
-      })
-      .catch((error) => {
-        return res.status(500).json({
-          status: "error",
-          message: "Error en la consulta"
-        })
-      })
-
-  
+        return res.status(200).json({
+          status: "succes",
+          message: "User resgister corrtley",
+          user: userStored,
+        });
+      });
+    })
+    .catch((error) => {
+      return res.status(500).json({
+        status: "error",
+        message: "Error en la consulta",
+      });
+    });
 };
 
 module.exports = {
   pruebaUsers,
-  register
+  register,
 };
